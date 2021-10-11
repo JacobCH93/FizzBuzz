d1221db730eba7921ad3c25dc19a74e404eefa67
EC2 Default User <ec2-user@ip-172-31-13-234.eu-west-2.compute.internal>
2021-10-11 11:21:36 +0000

    FizzBuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,11 @@
-def fizzbuzz
-  
+def fizzbuzz(number)
+    if number % 3 == 0 && number % 5 == 0 
+      return 'fizzbuzz'
+    elsif number % 3 == 0 
+      return 'fizz'
+    elsif number % 5 == 0 
+      return 'buzz'
+    else 
+        return number
+    end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,13 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
-end
+     it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" for the number 15' do
+  expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+	   it 'returns "8" when passed 8' do
+    expect(fizzbuzz(8)).to eq 8
+  end
+  end
